5745c9d50563295363fa2496cb74e4658f7fff63
BinhAn14 <an100277@donga.edu.vn>
2025-05-17 13:07:22 +0700

    update bttuan2

---
+++ bttuan2/src/main/java/com/example/bttuan2/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.bttuan2.repository;
+import com.example.bttuan2.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
+
