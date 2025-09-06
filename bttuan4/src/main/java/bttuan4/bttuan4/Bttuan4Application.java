e7d68aec75f8ca9ef2ddb876aebade6d38b18b4b
Thái Văn Phúc <phuc100662@donga.edu.vn>
2025-09-06 08:44:20 +0700

    create project

---
+++ bttuan4/src/main/java/bttuan4/bttuan4/Bttuan4Application.java
@@ -0,0 +1,13 @@
+package bttuan4.bttuan4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bttuan4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bttuan4Application.class, args);
+	}
+
+}
