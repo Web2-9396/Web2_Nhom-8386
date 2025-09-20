16706ca3fa7576765c4ac98db47aab7fea875708
Thai Van Phuc <phuc100662@donga.edu.vn>
2025-09-20 09:52:05 +0700

    create btvn tuan 6

---
+++ btvn_tuan6/src/test/java/com/example/btvn_tuan6/BtvnTuan6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btvn_tuan6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtvnTuan6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
